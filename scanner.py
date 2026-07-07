0641ebc60ccf6345c2d35005ec86680814190a1b
tharu-nsbm <tawrajapaksha@students.nsbm.ac.lk>
2026-07-07 23:43:02 +0530

    Added custom port range scanning

--- scanner.py
+++ scanner.py
@@ -2,12 +2,16 @@ import socket
 
 target = input("Enter target IP: ")
 
+start_port = int(input("Enter start port: "))
+end_port = int(input("Enter end port: "))
+
 print(f"\nScanning target: {target}")
+print(f"Port Range: {start_port} - {end_port}")
 print("-" * 30)
 
 open_ports = 0
 
-for port in range(20, 100):
+for port in range(start_port, end_port + 1):
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
